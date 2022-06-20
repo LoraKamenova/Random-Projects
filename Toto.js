e983d84961c562751599020ce9ec765e78f71106
Lora <cruzeiro@abv.bg>
2022-06-20 20:32:29 +0300

    Initial commit

--- Toto.js
+++ Toto.js
@@ -1,5 +1,14 @@
 function solve () {
-    let drawnNumbers = [];
-    let historyNumbers = [];
+    const arr = [5, 5, 5, 2, 2, 2, 2, 2, 9, 4];
+    const counts = {};
+
+    for (const num of arr) {
+        counts[num] = counts[num] ? counts[num] + 1 : 1;
+    }
+
+    console.log(counts[5], counts[2], counts[9], counts[4]);
+    console.log(counts[5]); // logs '3'
 }
 solve();
+
+
