ae9112fcbf36161a25b823ec1ff4e8c8e23df4b2
Lora <cruzeiro@abv.bg>
2022-06-25 16:38:37 +0300

    Initial commit

--- Toto/Script_5_of_35.js
+++ Toto/Script_5_of_35.js
@@ -1,12 +1,17 @@
+let fs = require('fs');
+let array = fs.readFileSync('5_of_35_draws.txt').toString().split("\r\n");
+let arr = [];
 
-const fs = require('fs')
+console.log(array);
 
-fs.readFile('5_of_35_draws.txt', 'utf-8', (err, data) => {
-    if (err) throw err;
+for (let i = 0; i < array.length - 1; i++) {
+    let myArray = array[i].split(" - ");
+    let temp = myArray[1].split(",");
 
-    console.log(data);
-})
+    for (let j = 0; j < temp.length; j++) {
+        arr.push(temp[j].trim());
+    }
+}
 
-
-
+console.log(arr);
 
