8ee8bc846deecf8bb2bab99271213ab4c4f4f80e
Lora <cruzeiro@abv.bg>
2022-06-25 16:13:46 +0300

    Initial commit

--- Toto/Script_5_of_35.js
+++ Toto/Script_5_of_35.js
@@ -1,8 +1,12 @@
 
 const fs = require('fs')
 
-fs.readFile('6_of_49_drawn_numbers.txt', 'utf-8', (err, data) => {
+fs.readFile('5_of_35_draws.txt', 'utf-8', (err, data) => {
     if (err) throw err;
 
     console.log(data);
 })
+
+
+
+
